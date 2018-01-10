ed5a9bada6b51bd04aa943bf194e77047a652140
jny <nerdrun.jny@gmail.com>
2018-01-11 02:00:50 +0900

    #3 홈화면 - changed layout ( req: please exactly pull !! Before commit )

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongo = require('mongodb');
 const port = 3000;
 
 app.set('views', __dirname + ('/views'));
 app.set('view engine', 'pug');
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(path.join(__dirname + '/public')));
 app.locals.pretty = true;
 
 app.get('/', ( req, res ) => {
