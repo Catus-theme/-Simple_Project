f5696e95c81cecb75e13f590f1141829d2344abf
nerdy-adams <nerdrun.adams@gmail.com>
2018-01-11 10:45:17 +0900

    #4 mongoDB 연결 - merged #3 홈화면

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const express = require('express');
 const app = express();
 const mongo = require('mongodb');
+// client server port 
 const port = 3000;
+// Connect URL
+const url = 'mogobd://localhost:27017';
+// Database Name
+const dbName = 'board';
+
+// Use connect method to connect to the server
+// MongoClient.connect(url,(err,client) => {
+//   // assert.equal(null,err);
+//   console.log('Connected successfully to server');
+//   const db = client.db(dbName);
+//   console.log('db :', db);
+//   client.close();
+// })
 
 app.set('views', __dirname + ('/views'));
 app.set('view engine', 'pug');
